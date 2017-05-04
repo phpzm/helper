343d540e84f2a45f17fbb86ec0c7e9791bb1618e
Ézio <ezioadsr@gmail.com>
2017-05-04 20:27:02 -0300

    Mudança da classe Datetime para aceitar valores do tipo date (YYYY-MM-dd)

--- src/Datetime.php
+++ src/Datetime.php
@@ -8,7 +8,7 @@ class Datetime extends \DateTime
      * @var string
      */
     private static $format = 'Y-m-d H:i:s';
-
+    private static $dateFormat = 'Y-m-d';
 
     /**
      * @param $date
@@ -16,7 +16,8 @@ class Datetime extends \DateTime
      */
     public static function isValid($date)
     {
-        $base = self::createFromFormat(static::$format, $date);
-        return $base instanceof \DateTime ? true: false;
+        $datetime = self::createFromFormat(static::$format, $date);
+        $date = self::createFromFormat(static::$dateFormat, $date);
+        return ($datetime instanceof \DateTime || $date instanceof \DateTime) ? true: false;
     }
 }
